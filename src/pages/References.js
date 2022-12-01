2f612b9c9b3a68b0f933ca035ed2266c8ef584a3
jcuribev <jcamilouribev@gmail.com>
2022-11-30 20:29:52 -0500

    Added references

--- src/pages/References.js
+++ src/pages/References.js
@@ -1,14 +1,24 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React, { Component } from 'react'
 
 export class References extends Component {
   render() {
     return (
-      <View>
-        <Text>References</Text>
+      <View style={{alignItems: 'center'}}>
+        <Text style={styles.text}>Jeschke MG, van Baar ME, Choudhry MA, Chung KK, Gibran NS, Logsetty S. Burn injury. Nat Rev Dis Primers. 2020 Feb 13;6(1):11. doi: 10.1038/s41572-020-0145-5. PMID: 32054846; PMCID: PMC7224101.</Text>
+        <Text style={styles.text}>https://www.who.int/news-room/fact-sheets/detail/burns</Text>
+        <Text style={styles.text}>Secretaría de Estado de la Seguridad Social Gobierno de España. (2020). MANUAL DE PRIMEROS AUXILIO EN LA EMPRESA. MC MUTUAL.</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    fontStyle: 'italic',
+    margin: 15
+  }
+})
 
 export default References
